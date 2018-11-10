09e975451892ae6772dc4f3312458c732c7a86fc
upquark00 <upquark00@users.noreply.github.com>
2018-11-09 17:31:35 -0800

    Update Deck_Complete.py

--- Deck_Complete.py
+++ Deck_Complete.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Apr 22 19:26:59 2018
+@author: Upquark00
 
-@author: Mira
+First version of a deck of cards built using an object-oriented approach. 
 """
 
 from random import shuffle, randint
